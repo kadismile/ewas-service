4370304e27431061881333809d073886e480a0f0
Ibrahim <ikadismile@gmail.com>
2023-12-06 09:31:38 +0000

    fixed dashboard

---
+++ front-end-new/src/pages/About.jsx
@@ -0,0 +1,58 @@
+
+export const About = ()=> {
+
+  return (
+    <>
+       <main className="main">
+        <section className="section-box">
+          <div className="breacrumb-cover bg-img-about">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-6">
+                  <h2 className="mb-10">About Us</h2>
+                  
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section-box mt-50">
+          <div className="post-loop-grid">
+            <div className="container">
+              <div className="text-center">
+                
+                <h2 className="section-title mb-10 wow animate__animated animate__fadeInUp">About Us</h2>
+                <p className="font-sm color-text-paragraph wow animate__animated animate__fadeInUp w-lg-50 mx-auto">
+                  This EWER system is dedicated to safeguarding lives and livelihoods through cutting-edge Early Warning
+                  and Early Response solutions. Our mission is to create a safer and more resilient world by providing
+                  timely, accurate, and actionable information when it matters most.
+                </p>
+              </div>
+              <div className="row mt-70">
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                  <img src="assets/imgs/page/about/img-about2.png" alt="joxBox" />
+                </div>
+                
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                  <h3 className="mt-15">What we can do?</h3>
+                  <div className="mt-20">
+                    <p className="font-md color-text-paragraph mt-20">Aenean sollicituin, lorem quis bibendum auctor nisi elit consequat ipsum sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet maurisorbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctora ornare odio.</p>
+                    <p className="font-md color-text-paragraph mt-20">Aenean sollicituin, lorem quis bibendum auctor nisi elit consequat ipsum sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet maurisorbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctora ornare odio.</p>
+                    <p className="font-md color-text-paragraph mt-20">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis non nisi purus. Integer sit nostra, per inceptos himenaeos.</p>
+                    <p className="font-md color-text-paragraph mt-20">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis non nisi purus. Integer sit nostra, per inceptos himenaeos.</p>
+                  </div>
+                  <div className="mt-30"><a className="btn btn-brand-1" href="#">Read More</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        
+       
+       
+        
+      </main>
+    </>
+  )
+}
